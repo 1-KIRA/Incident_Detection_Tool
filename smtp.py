507d90a1b8659cb9a40c3e1a7fe9f6ab64693eb0
Nikhil Acharya <101514927+1-KIRA@users.noreply.github.com>
2023-03-21 05:29:13 +0000

    Sending alerts and sending incidents logs to elasticsearch added

---
+++ smtp.py
@@ -0,0 +1,32 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+class GmailSender:
+    def __init__(self, credentials_file):
+        with open(credentials_file, 'r') as f:
+            self.smtp_username, self.smtp_password, self.to_address = f.read().splitlines()
+
+        self.smtp_server = 'smtp.gmail.com'
+        self.smtp_port = 587
+    
+    def send_email(self, subject, body):
+        # create a message object
+        msg = MIMEMultipart()
+        msg['From'] = self.smtp_username
+        msg['To'] = self.to_address
+        msg['Subject'] = subject
+
+        # add some text to the message
+        text = body
+        msg.attach(MIMEText(text))
+
+        # send the message
+        with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+            server.starttls()
+            server.login(self.smtp_username, self.smtp_password)
+            server.sendmail(self.smtp_username, self.to_address, msg.as_string())
+
+        print('Email sent!')
+# sender = GmailSender('env.txt')
+# sender.send_email('Test email from Python', 'Hello, this is a test email from Python.')
--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -5,56 +5,53 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from smtp import GmailSender
 
-class HttpBruteforce:
+class SshBruteforce:
     def __init__(self, rules_file_path, elasticsearch_hosts):
-    # Load YAML rule from file
+        # Load YAML rule from file
         with open(rules_file_path, 'r') as f:
             self.rule = yaml.safe_load(f)
+        
         # Initialize data structure to store failed login attempts
         self.ip_attempts = defaultdict(int)
-        self.http_url_attempts= defaultdict(int)
         self.ip_last_attempt_time = {}
-
-
+        
         # Connect to Elasticsearch
         self.es = Elasticsearch(elasticsearch_hosts)
-
+        
     def process_logs(self, index_name):
-        query = Q('exists', field='username_pass')
+        # Elasticsearch query to retrieve log entries
+        query = Q('match', action='Failed password for') & Q('exists', field='IPV4')
         s = Search(using=self.es, index=index_name).query(query)
 
         # Process log entries
         for hit in s.scan():
             # Extract relevant fields (e.g., IP address, timestamp)
-            http_ip_address = hit.client
-            timestamp = datetime.datetime.strptime(hit['timestamp'], '%d/%b/%Y:%H:%M:%S %z')
-            http_url=hit.url
+            ip_address = hit.IPV4
+            timestamp = datetime.datetime.strptime(hit['Timestamp'], '%b %d %H:%M:%S')
+            hostname=hit.hostname
+            user=hit.User
             # Check if failed login attempt matches the rule
-            if http_ip_address and http_url:
+            if ip_address:
                 # Update data structure with new failed login attempt
-                self.ip_attempts[http_ip_address] += 1
-                self.http_url_attempts[http_url] +=1
-
+                self.ip_attempts[ip_address] += 1
                 # Check if the IP address has reached the threshold in the specified timeframe
-                if self.ip_attempts[http_ip_address] >= self.rule['conditions'][1]['http_count']:
-                    if http_ip_address in self.ip_last_attempt_time and \
-                    (timestamp - self.ip_last_attempt_time[http_ip_address]).seconds <= self.rule['conditions'][1]['http_timeframe']:
-                        alert=(f"Incident detected: {http_ip_address} made {self.ip_attempts[http_ip_address]} failed login attempts within {self.rule['conditions'][1]['http_timeframe']} seconds in {http_url}.")
+                if self.ip_attempts[ip_address] >= self.rule['conditions'][0]['count']:
+                    if ip_address in self.ip_last_attempt_time and \
+                       (timestamp - self.ip_last_attempt_time[ip_address]).seconds <= self.rule['conditions'][0]['timeframe']:
+                        alert=(f"Incident detected: {ip_address} made {self.ip_attempts[ip_address]} failed login attempts within {self.rule['conditions'][0]['timeframe']} seconds in {hostname} for user {user}.")
                         # Clear the number of attempts for this IP
-                        self.ip_attempts[http_ip_address] = 0
+                        self.ip_attempts[ip_address] = 0
                         sender = GmailSender('env.txt')
                         sender.send_email('Incident Detected', alert)
                         send_log_index='incident'
                         detected_incident={'message':alert}
                         self.es.index(index=send_log_index, document=detected_incident)
-                        break
                     else:
                         # Update last attempted time for the IP
-                        self.ip_last_attempt_time[http_ip_address] = timestamp
-                        self.ip_attempts[http_ip_address] = 1
-                    
-    # Create an instance of RuleEnginea
-engine = HttpBruteforce('rules.yaml', ['http://3.229.13.155:9200'])
+                        self.ip_last_attempt_time[ip_address] = timestamp
+                        self.ip_attempts[ip_address] = 1
+
+engine = SshBruteforce('rules.yaml', ['http://3.229.13.155:9200'])
 
 # Call the process_logs method to run the rule engine
-engine.process_logs('access_log')
+engine.process_logs('test')
