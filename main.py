3f4f8b35f13691794da6c06164544f6725933e84
1-KIRA <acharyanikhil89@gmail.com>
2023-04-07 16:34:53 +0545

    Added Error handling

--- main.py
+++ main.py
@@ -5,39 +5,48 @@ from elasticsearch import exceptions
 from multiprocessing import Process
 import sys
 
+if exceptions.ConnectionError:
+    print("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    print('Elascticsearch not connected. Elasticsearch seems down. \n')
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    sys.exit()
 
-# Define functions to run the engine processes
-def run_http_engine():
-    HttpEngine = HttpBruteforce('rules.yaml', ['http://3.229.13.155:9200'])
-    while True:
-        HttpEngine.process_apache_logs('access_log')
-        print('HTTP engine running...')
+else:
+    try:
+        # Define functions to run the engine processes
+        def run_http_engine():
+            HttpEngine = HttpBruteforce('rules.yaml', ['http://3.229.13.155:9200'])
+            while True:
+                HttpEngine.process_apache_logs('access_log')
+                print('HTTP engine running...')
 
-def run_ssh_engine():
-    SshEngine = SshBruteforce('rules.yaml', ['http://3.229.13.155:9200'])
-    while True:
-        SshEngine.process_auth_logs('test')
-        print('SSH engine running...')
+        def run_ssh_engine():
+            SshEngine = SshBruteforce('rules.yaml', ['http://3.229.13.155:9200'])
+            while True:
+                SshEngine.process_auth_logs('test')
+                print('SSH engine running...')
 
-def run_dos_engine():
-    DosEngine = DOS('rules.yaml', ['http://3.229.13.155:9200'])
-    while True:
-        DosEngine.process_kern_logs('doslog')
-        print('DOS engine running...')
+        def run_dos_engine():
+            DosEngine = DOS('rules.yaml', ['http://3.229.13.155:9200'])
+            while True:
+                DosEngine.process_kern_logs('doslog')
+                print('DOS engine running...')
 
 
-if __name__ == '__main__':
-    # Create separate processes for each engine
-    http_process = Process(target=run_http_engine)
-    ssh_process = Process(target=run_ssh_engine)
-    dos_process = Process(target=run_dos_engine)
-    
-    # Start the processes
-    http_process.start()
-    ssh_process.start()
-    dos_process.start()
+        if __name__ == '__main__':
+            # Create separate processes for each engine
+            http_process = Process(target=run_http_engine)
+            ssh_process = Process(target=run_ssh_engine)
+            dos_process = Process(target=run_dos_engine)
+            
+            # Start the processes
+            http_process.start()
+            ssh_process.start()
+            dos_process.start()
 
-    # Wait for the processes to finish
-    http_process.join()
-    ssh_process.join()
-    dos_process.join()
+            # Wait for the processes to finish
+            http_process.join()
+            ssh_process.join()
+            dos_process.join()
+    except KeyboardInterrupt:
+        sys.exit()
